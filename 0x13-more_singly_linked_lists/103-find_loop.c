9038728321e3c968c65c30a283d7d86328d7d303
nadertoti <nadertoti4@gmail.com>
2023-09-25 12:22:25 -0700

    test

--- 0x13-more_singly_linked_lists/103-find_loop.c
+++ 0x13-more_singly_linked_lists/103-find_loop.c
@@ -1,33 +1,34 @@
 #include "lists.h"
 
 /**
- * find_listint_loop - Finds the loop in a linked list using Floyd's Algorithm.
- * @head: A pointer to the head of the list.
+ * find_listint_loop - Finds the loop in a linked list.
+ * @head: Pointer to the linked list to search for a loop.
  *
- * Return: The address of the node where the loop starts, or NULL if there's no loop.
+ * Return: Address of the node where the loop starts, or NULL if there's no loop.
  */
 listint_t *find_listint_loop(listint_t *head)
 {
     listint_t *slow = head;
     listint_t *fast = head;
 
-    while (slow != NULL && fast != NULL && fast->next != NULL)
-    {
-        slow = slow->next;          /* Move one step at a time */
-        fast = fast->next->next;    /* Move two steps at a time */
+    if (!head)
+        return (NULL);
 
-        if (slow == fast) /* Loop detected */
+    while (slow && fast && fast->next)
+    {
+        fast = fast->next->next;
+        slow = slow->next;
+        if (fast == slow)
         {
-            /* Move one pointer to the head and continue at the same pace */
             slow = head;
             while (slow != fast)
             {
                 slow = slow->next;
                 fast = fast->next;
             }
-            return (slow); /* Return the starting node of the loop */
+            return (fast);
         }
     }
 
-    return (NULL); /* No loop found */
+    return (NULL);
 }
