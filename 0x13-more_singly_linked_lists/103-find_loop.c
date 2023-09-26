cdb3f93db1f36315a977cdb219a1fa18b12c3afd
nadertoti <nadertoti4@gmail.com>
2023-09-26 09:13:39 -0700

    103-find_loop.c

--- 0x13-more_singly_linked_lists/103-find_loop.c
+++ 0x13-more_singly_linked_lists/103-find_loop.c
@@ -1,10 +1,10 @@
 #include "lists.h"
 
 /**
- * find_listint_loop - Finds the loop in a linked list.
- * @head: Pointer to the linked list to search for a loop.
+ * find_listint_loop - finds the loop in a linked list
+ * @head: linked list to search for
  *
- * Return: Address of the node where the loop starts, or NULL if there's no loop.
+ * Return: address of the node where the loop starts, or NULL
  */
 listint_t *find_listint_loop(listint_t *head)
 {
@@ -31,5 +31,4 @@ listint_t *find_listint_loop(listint_t *head)
 	}
 
 	return (NULL);
-
 }
