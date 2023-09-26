ef2e1bb3a7084847d8349809876f884d5c9f9e01
nadertoti <nadertoti4@gmail.com>
2023-09-26 07:49:33 -0700

    102-free_listint_safe.c

--- 0x13-more_singly_linked_lists/102-free_listint_safe.c
+++ 0x13-more_singly_linked_lists/102-free_listint_safe.c
@@ -29,7 +29,7 @@ size_t free_listint_safe(listint_t **h)
         else
         {
             free(*h);
-            *h = NULL;
+                       *h = NULL;
             len++;
             break;
         }
