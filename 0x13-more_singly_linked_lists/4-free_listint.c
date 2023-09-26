5c3efc38e9ddca6a78d18aeab3ddefa518183358
nadertoti <nadertoti4@gmail.com>
2023-09-26 05:49:20 -0700

    4-free_listint.c

--- 0x13-more_singly_linked_lists/4-free_listint.c
+++ 0x13-more_singly_linked_lists/4-free_listint.c
@@ -6,16 +6,12 @@
  */
 void free_listint(listint_t *head)
 {
-  
     listint_t *temp;
 
     while (head)
     {
-      
         temp = head->next;
         free(head);
         head = temp;
-	
     }
-    
 }
