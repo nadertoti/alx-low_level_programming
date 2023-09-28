0de9c1e5ec9fb9e9b307eb6de287c0419e608e60
nadertoti <nadertoti4@gmail.com>
2023-09-28 07:10:24 -0700

    1-print_binary.c

--- 0x14-bit_manipulation/1-print_binary.c
+++ 0x14-bit_manipulation/1-print_binary.c
@@ -6,19 +6,19 @@
  */
 void print_binary(unsigned long int n)
 {
-    int shift;
-    int flag = 0;
+	int shift;
+	int flag = 0;
 
-    for (shift = sizeof(n) * 8 - 1; shift >= 0; shift--)
-    {
-        if ((n >> shift) & 1)
-        {
-            _putchar('1');
-            flag = 1;
-        }
-        else if (flag || shift == 0)
-        {
-            _putchar('0');
-        }
-    }
+	for (shift = sizeof(n) * 8 - 1; shift >= 0; shift--)
+	{
+		if ((n >> shift) & 1)
+		{
+			_putchar('1');
+			flag = 1;
+		}
+		else if (flag || shift == 0)
+		{
+			_putchar('0');
+		}
+	}
 }
