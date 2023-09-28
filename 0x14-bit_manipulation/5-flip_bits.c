ee7037bd7b739cacb64035189dc1c6a987a6aecd
nadertoti <nadertoti4@gmail.com>
2023-09-28 09:51:52 -0700

    5-flip_bits.c

--- 0x14-bit_manipulation/5-flip_bits.c
+++ 0x14-bit_manipulation/5-flip_bits.c
@@ -13,10 +13,11 @@ unsigned int flip_bits(unsigned long int n, unsigned long int m)
 	unsigned long int diff = n ^ m;
 	unsigned int count = 0;
 
-	while (diff > 0)
-	{
-		count += diff & 1; /* Check the least significant bit */
-		diff >>= 1;       /* Right-shift to examine the next bit */
+	while (diff)
+        {
+	        if (diff & 1)
+			count++;
+		diff = diff >> 1;
 	}
 
 	return count;
