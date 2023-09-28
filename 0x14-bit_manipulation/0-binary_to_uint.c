017ebd82c6ae7476b941bcb637e9c32dcf5c259c
nadertoti <nadertoti4@gmail.com>
2023-09-28 06:51:19 -0700

    0-binary_to_uint.c

--- 0x14-bit_manipulation/0-binary_to_uint.c
+++ 0x14-bit_manipulation/0-binary_to_uint.c
@@ -10,19 +10,19 @@
  */
 unsigned int binary_to_uint(const char *b)
 {
-    unsigned int result = 0;
+	unsigned int result = 0;
 
-    if (b == NULL)
-        return 0;
+	if (b == NULL)
+		return (0);
 
-    while (*b)
-    {
-        if (*b != '0' && *b != '1')
-            return 0; /* Invalid character found, return 0 */
+	while (*b)
+	{
+		if (*b != '0' && *b != '1')
+			return (0); /* Invalid character found, return 0 */
 
-        result = (result << 1) | (*b - '0'); /* Convert binary to integer */
-        b++;
-    }
+		result = (result << 1) | (*b - '0'); /* Convert binary to integer */
+		b++;
+	}
 
-    return result;
+	return (result);
 }
