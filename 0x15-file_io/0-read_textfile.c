ddb8b2a9e4f5c8d15a99212720a5e6fb4435d6a8
nadertoti <nadertoti4@gmail.com>
2023-10-06 09:16:23 -0700

    0-read_textfile.c

--- 0x15-file_io/0-read_textfile.c
+++ 0x15-file_io/0-read_textfile.c
@@ -15,44 +15,44 @@
  */
 ssize_t read_textfile(const char *filename, size_t letters)
 {
-	int fd;
-	char *buffer;
-	ssize_t bytes_read, bytes_written;
+    int fd;
+    char *buffer;
+    ssize_t bytes_read, bytes_written;
 
-	if (filename == NULL)
-		return (0);
+    if (filename == NULL)
+        return (0);
 
-	fd = open(filename, O_RDONLY);
-	if (fd == -1)
-		return (0);
+    fd = open(filename, O_RDONLY);
+    if (fd == -1)
+        return (0);
 
-	buffer = malloc(letters);
-	if (buffer == NULL)
-	{
-		close(fd);
-		return (0);
-	}
+    buffer = malloc(letters);
+    if (buffer == NULL)
+    {
+        close(fd);
+        return (0);
+    }
 
-	bytes_read = read(fd, buffer, letters);
+    bytes_read = read(fd, buffer, letters);
 
-	if (bytes_read == -1)
-	{
-		close(fd);
-		free(buffer);
-		return (0);
-	}
+    if (bytes_read == -1)
+    {
+        close(fd);
+        free(buffer);
+        return (0);
+    }
 
-	bytes_written = write(STDOUT_FILENO, buffer, bytes_read);
+    bytes_written = write(STDOUT_FILENO, buffer, bytes_read);
 
-	if (bytes_written != bytes_read)
-	{
-		close(fd);
-		free(buffer);
-		return (0);
-	}
+    if (bytes_written != bytes_read)
+    {
+        close(fd);
+        free(buffer);
+        return (0);
+    }
 
-	close(fd);
-	free(buffer);
+    close(fd);
+    free(buffer);
 
-	return (bytes_read);
+    return (bytes_read);
 }
