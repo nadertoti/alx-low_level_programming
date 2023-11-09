3418d4c3b440838ae81667b6c7c48864b80e5508
nadertoti <nadertoti4@gmail.com>
2023-11-09 08:17:27 -0800

    0-print_dlistint.c

--- 0x17-doubly_linked_lists/0-print_dlistint.c
+++ 0x17-doubly_linked_lists/0-print_dlistint.c
@@ -12,7 +12,7 @@ size_t print_dlistint(const dlistint_t *h)
 
   while (h)
     {
-      printf("%d\n", j->n);
+      printf("%d\n", h->n);
       h = h->next;
       i++;
     }
--- 0x17-doubly_linked_lists/lists.h
+++ 0x17-doubly_linked_lists/lists.h
@@ -1,7 +1,11 @@
+#ifndef LISTS_H
+#define LISTS_H
+
+
 #include <stdlib.h>
 #include <string.h>
 #include <stdio.h>
-#include "lists.h"
+
 
 /**
  * struct dlistint_s - doubly linked list
@@ -19,3 +23,5 @@ typedef struct dlistint_s
     struct dlistint_s *next;
 } dlistint_t;
 size_t print_dlistint(const dlistint_t *h);
+
+#endif
