0ce8a3772fd318609f4f848f4913bec56da3a3cd
nadertoti <nadertoti4@gmail.com>
2023-11-09 08:20:04 -0800

    0-print_dlistint.c

--- 0x17-doubly_linked_lists/0-print_dlistint.c
+++ 0x17-doubly_linked_lists/0-print_dlistint.c
@@ -1,20 +1,20 @@
 #include "lists.h"
 
-/*
+/**
  * print_dlistint - prints doubly-linked list
- * @h: address
+ * @h: address of the head of the list
  *
- * Return: size
+ * Return: size of the list
  */
 size_t print_dlistint(const dlistint_t *h)
 {
-  size_t i = 0;
+    size_t i = 0;
 
-  while (h)
+    while (h)
     {
-      printf("%d\n", h->n);
-      h = h->next;
-      i++;
+        printf("%d\n", h->n);
+        h = h->next;
+        i++;
     }
-  return (i);
+    return (i);
 }
