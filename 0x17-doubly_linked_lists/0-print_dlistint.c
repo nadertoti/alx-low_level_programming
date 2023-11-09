ee2b7ad7327ad81a250ebe7aa4ae7bfc9e97fc0b
nadertoti <nadertoti4@gmail.com>
2023-11-09 08:11:09 -0800

    0-print_dlistint.c

---
+++ 0x17-doubly_linked_lists/0-print_dlistint.c
@@ -0,0 +1,20 @@
+#include "lists.h"
+
+/*
+ * print_dlistint - prints doubly-linked list
+ * @h: address
+ *
+ * Return: size
+ */
+size_t print_dlistint(const dlistint_t *h)
+{
+  size_t i = 0;
+
+  while (h)
+    {
+      printf("%d\n", j->n);
+      h = h->next;
+      i++;
+    }
+  return (i);
+}
